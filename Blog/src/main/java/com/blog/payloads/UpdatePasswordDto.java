7f8c17a52e1a94d618a7ed5e6c17c6218cb393eb
Divya1804 <pateldivya1804@gmail.com>
2024-02-15 02:39:30 +0530

    User API Created

---
+++ Blog/src/main/java/com/blog/payloads/UpdatePasswordDto.java
@@ -0,0 +1,18 @@
+package com.blog.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdatePasswordDto {
+
+    private String password;
+    private String newPassword;
+    private String confirmPassword;
+
+}
