d9432e8f789b33b50b6af7151a8c537010630818
Divya1804 <pateldivya1804@gmail.com>
2024-02-20 22:19:59 +0530

    Jwt Authentication Update

---
+++ Blog/src/main/java/com/blog/jwt/JwtTokenHelper.java
@@ -0,0 +1,58 @@
+package com.blog.jwt;
+
+import com.blog.payloads.UserDto;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenHelper {
+
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    private String secret = "jwtTokenKey";
+
+    public String getUsernameFromToken(String token){
+        return getClaimForToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token){
+        return getClaimForToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimForToken(String token, Function<Claims, T> claimResolver){
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token){
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(UserDto userDto){
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDto.getUserName());
+    }
+
+    public String doGenerateToken(Map<String, Object> claims, String subject){
+        return Jwts.builder().setClaims(claims).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 10000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+    public Boolean validateToken(String token, UserDto userDto){
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDto.getUserName()) && !isTokenExpired(token));
+    }
+
+}
